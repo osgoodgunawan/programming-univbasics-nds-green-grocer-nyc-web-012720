cfb79c3a227b9f400fce3f6addd976996dde489d
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-19 05:45:38 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -35,7 +35,9 @@ def apply_coupons(cart, coupons)
     cart_item=find_item_by_name_in_collection(coupons[counter][:item],cart)
    coupon_name="#{coupons[counter][:item]} W/COUPON"
    cart_item_with_coupon=find_item_by_name_in_collection(coupon_name,cart)
+   if
    
+  end
   
   end
 
