b57dac7b6af4151f6ac8a2ab0e69a6ca293d8a23
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-19 06:07:37 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -47,7 +47,8 @@ def apply_coupons(cart, coupons)
          :count => coupons[counter][:num]
        }
        cart << cart_item_with_coupon
-       cart_item[:count]-=
+       
+       cart_item[:count]-=coupons[counter][:num]
    end
   end
   counter +=1
