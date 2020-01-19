9aee8a847643738c362ca783fcf622e3e48b0a2a
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-19 05:52:51 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -36,7 +36,9 @@ def apply_coupons(cart, coupons)
    coupon_name="#{coupons[counter][:item]} W/COUPON"
    cart_item_with_coupon=find_item_by_name_in_collection(coupon_name,cart)
    if cart_item && cart_item[:count]>=coupons[counter][:num]
-     
+     if cart_item_with_coupon
+       cart_item_with_coupon[:count]=coupons[counter][:num]
+     else
    
   end
   counter +=1
